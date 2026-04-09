b77fa3bff56114cdd73c6d6f731bd98d53d62636
M Ceylan Morgul <mm4uz@virginia.edu>
2026-04-09 10:20:02 -0400

    Update signals-with-noise.py

--- Module 7 - Signal Processing/0 - Lecture Examples/signals-with-noise.py
+++ Module 7 - Signal Processing/0 - Lecture Examples/signals-with-noise.py
@@ -15,17 +15,17 @@ source_signal_frequency = 15
 source_signal_period = 1 / source_signal_frequency
 
 """
-Corrupting Signal
+Noise
 """
 
 # signal amplitude (Unitless)
-corrupt_amplitude = 1.1
+noise_amplitude = 1.1
 
 # signal frequency (Hertz)
-corrupt_signal_frequency = 60
+noise_signal_frequency = 60
 
 # from the frequency find the period (seconds)
-corrupt_signal_period = 1 / corrupt_signal_frequency
+noise_signal_period = 1 / noise_signal_frequency
 
 """
 Global Parameters
@@ -35,19 +35,19 @@ Global Parameters
 sampling_rate = 1000
 
 # make a simple plot of the signal sampled at the rate
-time = np.arange(start=0, stop=20 / min(corrupt_signal_frequency, source_signal_frequency), step=1 / sampling_rate)
+time = np.arange(start=0, stop=20 / min(noise_signal_frequency, source_signal_frequency), step=1 / sampling_rate)
 
 # now calculate the signal from the resulting sample rate
 # signal = Asin(2*pi*f*time+ phase)
 source_signal = source_amplitude * np.sin(2 * math.pi * source_signal_frequency * time)
-corrupt_signal = corrupt_amplitude * np.sin(2 * math.pi * corrupt_signal_frequency * time)
+noise_signal = noise_amplitude * np.sin(2 * math.pi * noise_signal_frequency * time)
 
 # plot the resulting signal in matplotlib
-plt.title('Base Signal with Corrupting Frequency')
+plt.title('Base Signal with noiseing Frequency')
 plt.xlabel('Time (s)')
 plt.ylabel('Signal')
 plt.plot(time, source_signal, label='Source Signal')
-plt.plot(time, corrupt_signal, label='Noise Signal')
-plt.plot(time, corrupt_signal + source_signal, label='Combined Signal')
+plt.plot(time, noise_signal, label='Noise Signal')
+plt.plot(time, noise_signal + source_signal, label='Combined Signal')
 plt.legend()
 plt.show()
